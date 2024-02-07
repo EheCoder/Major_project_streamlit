ba36250f581a5bbb2c14d7b4adf8b16dd81a469a
EheCoder <anvesh23628@gmail.com>
2024-02-07 19:42:59 +0530

    added comments

--- sandboxCaller.py
+++ sandboxCaller.py
@@ -4,3 +4,11 @@ l1=["aaaa","bbbb","cccc"]
 l2=["A","BB","CCC"]
 
 sandbox_codeEditor("quest1",'sbdajdsdvshdsvsfv sfbvdbf sbdajdsdvshdsvsfv sfbvdbf sbdajdsdvshdsvsfv sfbvdbf sbdajdsdvshdsvsfv sfbvdbf sbdajdsd',l1,l2)
+
+#this is a simple example to run sandbox.py
+#the args in sandbox_codeEditor func are:
+# question- string 
+# explanation- string 
+# example_list- list of strings 
+# content_list_of_sidebar- not sure yet takes strings, lists....
+# initial_text_in_editor- string
